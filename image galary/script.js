10cace587817d2741e0fc7c1a25361c85666dc0d
Raushan kumar saw <124686068+Raushan102@users.noreply.github.com>
2023-08-09 14:00:25 +0530

    Add files via upload

---
+++ image galary/script.js
@@ -0,0 +1,11 @@
+
+
+   
+        
+function big_imageFunction(SmallPicture)
+{   
+    let imageView=document.getElementById('big-view_of_image')
+    let BigImageContainer=document.getElementById('Big-Image-Container')
+    imageView.src=SmallPicture.src;
+}
+
